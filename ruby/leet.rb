edc0c76d867b93ecb8b8a0996d72b4b1b34bc91d
ybnr1130 <muuckk@gmail.com>
2021-08-30 07:41:25 +0000

    [Finish]

---
+++ ruby/leet.rb
@@ -0,0 +1,29 @@
+input_line = gets
+strings = input_line.to_s.split('')
+
+convert = []
+strings.each do |chara|
+  case chara
+    when "A" then
+    # 処理
+    convert.push("4")
+    when "E" then
+    # 処理
+    convert.push("3")
+    when "G" then
+    # 処理
+    convert.push("6")
+    when "I" then
+    # 処理
+    convert.push("1")
+    when "O" then
+    # 処理
+    convert.push("0")
+    else
+    # 処理
+    convert.push(chara)
+  end
+end
+
+output = convert.join
+puts output
